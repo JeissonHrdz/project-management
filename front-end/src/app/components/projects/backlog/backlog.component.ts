d487afa119534347610ce1f5dc1e7939034f8f70
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-05-26 11:07:24 -0500

    STYLE: table backlog styles

--- front-end/src/app/components/projects/backlog/backlog.component.ts
+++ front-end/src/app/components/projects/backlog/backlog.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { heroArrowTurnDownRight, heroEllipsisHorizontal } from '@ng-icons/heroicons/outline'; 
 
 @Component({
   selector: 'app-backlog',
-  imports: [],
+  imports: [ NgIcon ],
+  providers: [provideIcons({ heroArrowTurnDownRight, heroEllipsisHorizontal })],
   templateUrl: './backlog.component.html',
   styleUrl: './backlog.component.css'
-})
+}) 
 export class BacklogComponent {
 
 }
