d7d9cfd3ef85f2c9c059c168cad36fbbd9253463
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-05-27 10:52:19 -0500

    STYLES: Add epic section

--- front-end/src/app/components/projects/backlog/backlog.component.ts
+++ front-end/src/app/components/projects/backlog/backlog.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { heroArrowTurnDownRight, heroEllipsisHorizontal } from '@ng-icons/heroicons/outline'; 
+import $ from 'jquery';
 
 @Component({
   selector: 'app-backlog',
@@ -10,5 +11,17 @@ import { heroArrowTurnDownRight, heroEllipsisHorizontal } from '@ng-icons/heroic
   styleUrl: './backlog.component.css'
 }) 
 export class BacklogComponent {
+
+
+  viewMenu(id: string): void {
+    const menu = document.getElementById(id);
+    if (menu) {
+      menu.classList.toggle('hidden');
+    }
+  }
+
+  showFormEpic(){
+    $("#form-epic").animate({ height: "toggle" }, "fast");
+  }
 
 }
