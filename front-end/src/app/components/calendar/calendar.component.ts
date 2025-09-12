6ac4478c4c4d6b3073ad1b6cb26108b3906de0af
JeissonHrdz <jeissonhrdz186@gmail.com>
2025-09-12 11:15:05 -0500

    UPATE: Calendar task show

--- front-end/src/app/components/calendar/calendar.component.ts
+++ front-end/src/app/components/calendar/calendar.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CalendarEvent } from '../../core/model/entity/calendar-event';
 import { CommonModule } from '@angular/common';
+import { CalendarService } from '../../services/calendar.service';
 
 @Component({
   selector: 'app-calendar',
@@ -10,10 +11,43 @@ import { CommonModule } from '@angular/common';
 })
 export class CalendarComponent {
 
+  private calendarService = inject(CalendarService);
+
   currentMonth: Date = new Date();
   daysInMonth: Date[] = [];
   weekDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   events: CalendarEvent[] = [];
+
+  ngOnInit(): void {
+    this.generateCalendar();
+   
+    this.calendarService.addEvent({
+      id: 1,
+      title: 'Event 1',
+      startDate: new Date('2025-09-02'),
+      endDate: this.addDayDate(new Date('2025-09-20')),
+      status: 'pending'
+    });
+
+    
+    this.calendarService.addEvent({
+      id: 2,
+      title: 'Event 2',
+      startDate: new Date('2025-09-05'),
+      endDate: this.addDayDate(new Date('2025-09-15')),
+      status: 'pending'
+    });
+
+    this.calendarService.events$.subscribe(events => {
+      this.events = events;
+    });
+  }
+
+  addDayDate(date: Date): Date{   
+    const plusDate = new Date(date);
+    plusDate.setDate(plusDate.getDate() + 1);
+    return plusDate;    
+  }
 
 
   generateCalendar(): void {
@@ -35,6 +69,12 @@ export class CalendarComponent {
     this.daysInMonth = days;
   }
 
+  private normalizeEndDate(date: Date): Date {
+    const normalized = new Date(date);
+    normalized.setHours(23, 59, 59, 999); // último milisegundo del día
+    return normalized;
+  }
+
   nextMonth(): void {
     this.currentMonth = new Date(this.currentMonth.getFullYear(), this.currentMonth.getMonth() + 1, 1);
     this.generateCalendar();
@@ -53,6 +93,30 @@ export class CalendarComponent {
   selectDay(day: Date): void {
     // this.selectedDate = day;
     // this.showModal = true;
+  }
+
+  getEventsForDay(day: Date): { event: CalendarEvent, position: 'start' | 'middle' | 'end' | 'single' }[] {
+    return this.events
+      .filter(ev => {
+        const start = new Date(ev.startDate);
+        const end = this.normalizeEndDate(new Date(ev.endDate ?? ev.startDate));
+        console.log(start, end);
+        return day >= start && day <= end;
+      })
+      .map(ev => {
+        const start = new Date(ev.startDate);
+        const end = this.normalizeEndDate(new Date(ev.endDate ?? ev.startDate));
+  
+        if (start.toDateString() === end.toDateString()) {
+          return { event: ev, position: 'single' };
+        } else if (day.toDateString() === start.toDateString()) {
+          return { event: ev, position: 'start' };
+        } else if (day.toDateString() === end.toDateString()) {
+          return { event: ev, position: 'end' };
+        } else {
+          return { event: ev, position: 'middle' };
+        }
+      });
   }
 
 }
--- front-end/src/app/services/calendar.service.ts
+++ front-end/src/app/services/calendar.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { CalendarEvent } from '../core/model/entity/calendar-event';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CalendarService {
 
-  constructor() { }
+  private eventsSubject = new BehaviorSubject<CalendarEvent[]>([]);
+  events$ = this.eventsSubject.asObservable();
+
+  private events: CalendarEvent[] = [];
+
+  addEvent(event: CalendarEvent): void {
+    this.events.push(event);
+    this.eventsSubject.next(this.events);
+  }
+
+  removeEvent(id: number): void {
+    this.events = this.events.filter(e => e.id !== id);
+    this.eventsSubject.next(this.events);
+  }
 }
